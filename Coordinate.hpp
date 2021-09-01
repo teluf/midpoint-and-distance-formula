ebae12ba13520410ee18c097935a8bef4c90a74b
teluf <64613767+teluf@users.noreply.github.com>
2021-09-01 16:51:30 -0700

    Update Coordinate.hpp

--- Coordinate.hpp
+++ Coordinate.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 class Coordinate {
     private:
@@ -7,7 +8,8 @@ class Coordinate {
         Coordinate(float nx, float ny);
         float getX();
         float getY();
-        Coordinate* midpoint(Coordinate *x);
+        float distance(Coordinate *buf);
+        Coordinate* midpoint(Coordinate *buf);
         void printDetails();
 };
 
@@ -24,12 +26,19 @@ float Coordinate::getY() {
     return this -> y;
 }
 
-void Coordinate::printDetails() {
-    std::cout << "(" << this -> x << ", " << this -> y << ")";
+float Coordinate::distance(Coordinate *buf) {
+    float x2_x1_sqrd = pow((buf -> getX() - this -> x), 2);
+    float y2_y1_sqrd = pow((buf -> getY() - this -> y), 2);
+    float numtobesqrtd = x2_x1_sqrd + y2_y1_sqrd;
+    return sqrt(numtobesqrtd);
 }
 
 Coordinate* Coordinate::midpoint(Coordinate *buf) {
     float mid_x = (this -> x + buf -> getX()) / 2;
     float mid_y = (this -> y + buf -> getY()) / 2;
     return new Coordinate(mid_x, mid_y);
+}
+
+void Coordinate::printDetails() {
+    std::cout << "(" << this -> x << ", " << this -> y << ")";
 }
